5db5b315398712d6e0f920d42fb852507d4c84c6
Min Joon Seo <minjoon@cs.washington.edu>
2014-04-13 18:59:14 -0700

    label recognizer added.

--- image/low_level.py
+++ image/low_level.py
@@ -78,7 +78,7 @@ class BinarizedSegmentation:
         segment_list = []
         
         if seg_alg == 'naive':
-            s = [[1,1,1],[1,1,1],[1,1,1]]
+            kernel = np.ones((3,3), np.uint8)
             labeled, nr_objects = ndimage.label(bin_img)
             slices = ndimage.find_objects(labeled)
             for idx, s in enumerate(slices):
@@ -100,30 +100,48 @@ class BinarizedSegmentation:
             del segment_list[largest_idx]
             self.label_seg_list = segment_list
 
+def label_resize(img, target_len=16):
+    ylen, xlen = np.shape(img)
+    if ylen > xlen:
+        new_ylen = target_len
+        new_xlen = int(round(float(xlen*target_len)/ylen))
+    else:
+        new_xlen = target_len
+        new_ylen = int(round(float(ylen*target_len)/xlen))
+    
+    target_img = np.ones((target_len,target_len))*255
+    resized_img = cv2.resize(img, (new_xlen,new_ylen))
+    target_img[:new_ylen,:new_xlen] = resized_img
+    return target_img
+
 '''
 Given a list of label segments, train OCR
 This is to be used by LabelRecognizer
 For now, just saves the labels and user needs to
 manually modify the csv file
 '''
-class LabelTrainer:
-    def __init__(self, path='data/ocr/', digit=6, csv_name='label.csv'):
+class LabelSaver:
+    def __init__(self, path='data/ocr/', digit=6, data_filename='data.csv', img_len=16):
         self.path = path
         self.digit = digit
-        self.csv_fh = open(path+csv_name, 'a')
+        self.data_fh = open(path+data_filename, 'ar')
+        self.img_len = img_len
         
         self.num = 1
     
-    def interactive_train(self, segment):
+    def interactive_save(self, img):
         name = self.next_name()
-        cv2.imwrite(self.path+name, segment.img)
+        cv2.imwrite(self.path+name, img)
         char = raw_input("Label for %s: " %name)
-        self.csv_fh.write("%s,%s\n" %(name,char))
-    
-    def train(self, segment, char): 
+        self.data_fh.write("%s,%s," %(name,char))
+        flat_img = label_resize(img).flatten()
+        self.data_fh.write(",".join([str(int(e)) for e in flat_img])+'\n')
+        
+        
+    def save(self, img, char): 
         # save segment to next_name
         name = self.next_name()
-        cv2.imwrite(self.path+name, segment.img)
+        cv2.imwrite(self.path+name, img)
         self.csv_fh.write('%s,%s' %(name,char))
     
     def next_name(self):
@@ -133,22 +151,61 @@ class LabelTrainer:
             name = ('{0:0%d}' %self.digit).format(self.num) + '.png'
         return name
     
+    '''
+    label outstanding files via interactive mode
+    '''
+    def label_files(self):
+        pass
+    
+'''
+'''
+class LabelRecognizer:
+    def __init__(self, path='data/ocr/', digit=6, data_filename='data.csv', img_len=16):
+        self.path =path
+        self.digit = digit
+        self.feature_list = []
+        self.char_list = []
+        self.img_len = img_len
+        
+        # initialize feature dictionary
+        data_fh = open(path+data_filename, 'r')
+        reader = csv.reader(data_fh, delimiter=',')
+        for row in reader:
+            self.char_list.append(row[1])
+            self.feature_list.append([int(e) for e in row[2:]])
+            
+        
+    def recognize(self, img):
+        new_img = label_resize(img, target_len=self.img_len) 
+        flat_img = new_img.flatten()
+        cost_list = [np.linalg.norm(np.array(feature)-flat_img) for feature in self.feature_list]
+        idx = np.argmin(cost_list)
+        return self.char_list[idx]
+        
     
     
-def main():
+def train():
     fig = plt.figure()
     ax = fig.add_subplot(111)
     img = open_img(sys.argv[1])
     bin_seg = BinarizedSegmentation(img)
+    
+    '''
     ax.imshow(bin_seg.dgm_seg.img, cmap=cm.Greys_r)
     plt.show()
     '''
-    label_trainer = LabelTrainer()
+    label_trainer = LabelSaver()
     for segment in bin_seg.label_seg_list:
-        label_trainer.interactive_train(segment)
-    '''
+        label_trainer.interactive_label(segment.img)
+        
+def test():
+    img = open_img(sys.argv[1])
+    bin_seg = BinarizedSegmentation(img)
+    label_recog = LabelRecognizer()
+    for segment in bin_seg.label_seg_list:
+        print label_recog.recognize(segment.img)
     
 if __name__ == '__main__':
-    main()
+    train()
     
     
